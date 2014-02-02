d0d836b7a563278eba99ea6de8a483af0c808593
Tuomas Tynkkynen <dezgeg@gmail.com>
2014-02-02 21:12:52 +0200

    Scaffold beer club + membership, add model relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
 
   has_many :ratings
+  has_many :memberships, dependent: :destroy
 end
