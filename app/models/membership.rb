71380e96e8f3d900d80f2b1dfac369ad6427d339
Tuomas Tynkkynen <dezgeg@gmail.com>
2014-02-02 21:36:42 +0200

    Ex 9.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,5 @@ class Membership < ActiveRecord::Base
 
   validates_presence_of :user
   validates_presence_of :beer_club
+  validates_uniqueness_of :beer_club_id, scope: :user_id, message: "You have already joined this club"
 end
