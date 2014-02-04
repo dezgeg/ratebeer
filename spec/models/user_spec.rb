b58c7b2086a257be1ca2bcdc8e6e0bf73e3ba6ba
Tuomas Tynkkynen <dezgeg@gmail.com>
2014-02-04 17:23:23 +0200

    Week 4 Ex 1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
   it "has the username set correctly" do
-    user = User.new username:"Pekka"
+    user = User.new username: "Pekka"
 
     user.username.should == "Pekka"
   end
@@ -15,7 +15,7 @@ describe User do
   end
 
   describe "with a proper password" do
-    let(:user){ User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1" }
+    let(:user) { User.create username: "Pekka", password: "Secret1", password_confirmation: "Secret1" }
 
     it "is saved" do
       expect(user).to be_valid
@@ -23,14 +23,30 @@ describe User do
     end
 
     it "and with two ratings, has the correct average rating" do
-      rating = Rating.new score:10
-      rating2 = Rating.new score:20
+      rating = Rating.new score: 10
+      rating2 = Rating.new score: 20
 
       user.ratings << rating
       user.ratings << rating2
 
       expect(user.ratings.count).to eq(2)
       expect(user.average_rating).to eq(15.0)
+    end
+  end
+
+  password_tests = {
+    'abcdefgh' => 'no numbers',
+    'aA1' => 'too short',
+  }
+
+  password_tests.each do |pass, reason|
+    describe "with an improper password '#{pass}' (#{reason})" do
+      let(:user) { User.create username: "Pekka", password: pass, password_confirmation: pass }
+
+      it "is not saved" do
+        expect(user).not_to be_valid
+        expect(User.count).to eq(0)
+      end
     end
   end
 end
